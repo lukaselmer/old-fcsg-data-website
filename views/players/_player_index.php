49ce94c14c0704d0c3ca164d77b1167a2fa02cf5
Lukas Elmer <lukas.elmer@gmail.com>
2009-07-30 18:58:04 +0000

    show / hide all

--- views/players/_player_index.php
+++ views/players/_player_index.php
@@ -1,7 +1,7 @@
 <div style="height: 4px; width: 10px;"></div>
 <div class="player rounded">
     <div class="inner">
-        <div style="position:absolute; width: 703px;">
+        <div style="position:absolute; width:703px;">
             <div class="fl">
                 <a href="<? echo url_for('players', 'index', Array('open_all' => 1)) ?>" onclick="show_all_player_descriptions(); return false;" style="text-decoration:none;">
                     <em>Details einblenden</em>
@@ -15,12 +15,15 @@
             </div>
             <? clearer(); ?>
         </div>
-        <div class="player-index">
-            <? foreach ($anchor_letters as $startchar) { ?>
-            <a href="#players-<? echo $startchar ?>" style="text-decoration:none;">
-                <em><? echo $startchar ?></em>
-            </a>
-            <? } ?>
+        <div style="width:703px;">
+            <div class="player-index" style="position: absolute; z-index:100; width:703px;">
+                <? foreach ($anchor_letters as $startchar) { ?>
+                <a href="#players-<? echo $startchar ?>" style="text-decoration:none;">
+                    <em><? echo $startchar ?></em>
+                </a>
+                <? } ?>
+            </div>
+            &nbsp;
         </div>
     </div>
 </div>
