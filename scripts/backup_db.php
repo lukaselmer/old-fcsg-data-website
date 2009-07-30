3d7d93b0f2c554f7336aac5268e5296eb2421a10
Lukas Elmer <lukas.elmer@gmail.com>
2009-07-30 12:27:12 +0000

    install mysql dumper

--- scripts/backup_db.php
+++ scripts/backup_db.php
@@ -1,12 +1,7 @@
 <?php
 
-include('general.php');
-
-$tableName  = 'players';
-$backupFile = getcwd().'/backup/mypet.sql';
-$backupFile = 'C:/instantrails20/php_apps/fcsg-data/scripts/backup/mypeta.sql';
-$query      = "SELECT * INTO DUMPFILE '$backupFile' FROM $tableName";
-$result = mysql_query($query);
+header("Location: /scripts/msd1.24/");
+exit;
 
 /*$str = trim(file_get_contents('reset.sql'));
 $str = str_replace("#{DB_NAME}", $cfg['mysql']['database'], $str);
