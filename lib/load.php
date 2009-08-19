c728dcbd5da0297c9fdc0b25ee8bc2c244c006bc
Lukas Elmer <lukas.elmer@gmail.com>
2009-08-19 14:24:08 +0000

    RSS Feed

--- lib/load.php
+++ lib/load.php
@@ -1,5 +1,6 @@
 <?php
 
 include 'models/db.php';
+include 'rss_parser/FeedForAll_XMLParser.inc.php';
 
 ?>
--- views/players/_startpage_content.php
+++ views/players/_startpage_content.php
@@ -1,8 +1,12 @@
-<? if(strlen($startpage_content->content) > 0){ ?>
+<? if(strlen($startpage_content->content) > 0) { ?>
 <div style="height: 4px; width: 10px;"></div>
 <div class="player rounded">
     <div class="inner">
-        <? echo htmlspecialchars_decode($startpage_content->content); ?>
+        <div class="fl" style="width: 550px;"><? echo htmlspecialchars_decode($startpage_content->content); ?></div>
+        <div class="fl rss" style="width: 150px; font-size: 0.8em;">
+            <? include("http://local.fcsg-data.ch/lib/rss_parser/rss2html.php?XMLFILE=http://feeds.feedburner.com/fcsg%3fformat%3dxml&TEMPLATE=http://local.fcsg-data.ch/lib/rss_parser/fcsg-template.html&MAXITEMS=3"); ?>
+        </div>
+        <? clearer(); ?>
     </div>
 </div>
 <? } ?>
